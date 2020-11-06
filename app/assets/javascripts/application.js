4827b8b31db37130a6bd42faa6334c56be41694e
toshinobu kokubu <kokubuempire@gmail.com>
2020-11-06 14:17:08 +0900

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require jquery/dist/jquery.js
