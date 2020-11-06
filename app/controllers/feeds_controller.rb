95d40bf0b77d936d918b8ef31971cf0235cea03a
toshinobu kokubu <kokubuempire@gmail.com>
2020-11-06 10:45:30 +0900

    確認画面搭載

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,77 @@
+class FeedsController < ApplicationController
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
+
+  # GET /feeds
+  # GET /feeds.json
+  def index
+    @feeds = Feed.all
+  end
+
+  # GET /feeds/1
+  # GET /feeds/1.json
+  def show
+  end
+
+  # GET /feeds/new
+  def new
+    @feed = Feed.new
+  end
+
+  def confirm
+    @feed = Feed.new(feed_params)
+    render :new if @feed.invalid?
+  end
+  # GET /feeds/1/edit
+  def edit
+  end
+
+  # POST /feeds
+  # POST /feeds.json
+  def create
+    @feed = Feed.new(feed_params)
+    if params[:back]
+      render :new
+    else
+      if @feed.save
+        redirect_to feeds_path, notice: "投稿を作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  # PATCH/PUT /feeds/1
+  # PATCH/PUT /feeds/1.json
+  def update
+    respond_to do |format|
+      if @feed.update(feed_params)
+        format.html { redirect_to @feed, notice: 'Feed was successfully updated.' }
+        format.json { render :show, status: :ok, location: @feed }
+      else
+        format.html { render :edit }
+        format.json { render json: @feed.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /feeds/1
+  # DELETE /feeds/1.json
+  def destroy
+    @feed.destroy
+    respond_to do |format|
+      format.html { redirect_to feeds_url, notice: 'Feed was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_feed
+      @feed = Feed.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def feed_params
+      params.require(:feed).permit(:title, :content, :image, :image_cache)
+    end
+end
