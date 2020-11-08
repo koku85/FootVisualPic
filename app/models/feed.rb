c3f0803d3444339fce66a629d5901d2610070c5f
toshinobu kokubu <kokubuempire@gmail.com>
2020-11-08 14:51:05 +0900

    init

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,4 +3,6 @@ class Feed < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
+  validates :image, presence: true
+  validates :user_id, presence: true
 end
