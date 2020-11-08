89867c7aaa131b477ff0b3d10a0b3dbc468e5993
toshinobu kokubu <kokubuempire@gmail.com>
2020-11-08 14:28:51 +0900

    turbolinks消去

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require rails-ujs
+//= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
